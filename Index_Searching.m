cd1513db43b364b96cdea985aefcad4ace40ae01
dinhkhanh <khanhdd85@gmail.com>
2015-03-02 17:16:31 +0100

    fix Closed_Form.m

--- Index_Searching.m
+++ Index_Searching.m
@@ -1,4 +1,10 @@
 function [ index ] = Index_Searching( value,index_array )
     index = binaraysearchasc(index_array,value);
+%     for i = 1:length(index_array)
+%         if value == index_array(i)
+%             index = i;
+%             return
+%         end
+%     end
 end
 
